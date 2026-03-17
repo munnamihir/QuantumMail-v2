6b2931a09305fd049813a5fdb1b49f5838dcbee2
Mihir Bommisetty <38711080+munnamihir@users.noreply.github.com>
2026-03-16 22:42:58 -0400

    Add QIE button and popup for user interaction

---
+++ portal/components/qie.js
@@ -0,0 +1,50 @@
+const button = document.createElement("div");
+
+button.id = "qie-button";
+button.innerText = "QIE";
+
+document.body.appendChild(button);
+
+const popup = document.createElement("div");
+popup.id = "qie-popup";
+
+popup.innerHTML = `
+<div class="qie-header">QuantumMail Intelligence Engine</div>
+<div id="qie-messages"></div>
+<input id="qie-input" placeholder="Ask QIE..." />
+<button id="qie-send">Ask</button>
+`;
+
+document.body.appendChild(popup);
+
+button.onclick = () => {
+  popup.style.display =
+    popup.style.display === "block"
+      ? "none"
+      : "block";
+};
+
+document.getElementById("qie-send").onclick = askQIE;
+
+async function askQIE(){
+
+  const question =
+    document.getElementById("qie-input").value;
+
+  const res = await fetch("/qie/query", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ question })
+  });
+
+  const data = await res.json();
+
+  const div =
+    document.getElementById("qie-messages");
+
+  div.innerHTML += `<p><b>You:</b> ${question}</p>`;
+  div.innerHTML += `<p><b>QIE:</b> ${data.answer}</p>`;
+
+}
