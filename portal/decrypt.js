b6af94a948c46d5d149c4ae2a43eca1b247bf300
Mihir Bommisetty <38711080+munnamihir@users.noreply.github.com>
2026-03-25 18:31:39 -0400

    Update decrypt.js

--- portal/decrypt.js
+++ portal/decrypt.js
@@ -105,7 +105,7 @@ function requestDecrypt() {
   window.postMessage(
     {
       source: "quantummail-portal",
-      type: "QM_LOGIN_AND_DECRYPT_RESULT",
+      type: "QM_LOGIN_AND_DECRYPT_REQUEST",
       msgId,
       serverBase: window.location.origin,
       orgId,
