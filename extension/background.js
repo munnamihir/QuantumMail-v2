1be501f71c31b29d1b37439f7e5c8f1ef9094b83
Mihir Bommisetty <38711080+munnamihir@users.noreply.github.com>
2026-03-30 16:05:52 -0400

    Update background.js

--- extension/background.js
+++ extension/background.js
@@ -170,9 +170,7 @@ async function loginAndStoreSession({ serverBase, orgId, username, password }) {
     throw new Error("Login failed: missing token/user.");
   }
 
-  // 🔐 Register device key
-  await ensureKeypairAndRegister(base, token, user.userId);
-
+  
   await setSession({ serverBase: base, token, user });
 
   return { base, token, user };
