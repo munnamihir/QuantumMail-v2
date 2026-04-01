5e65589c871ba22ff6e0ff9b07e06b1872014cd7
Mihir Bommisetty <38711080+munnamihir@users.noreply.github.com>
2026-03-31 21:07:04 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-
+app.use(express.json())
 /* =========================================================
    ENV (Render / Neon)
 ========================================================= */
