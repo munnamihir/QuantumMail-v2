95f2a10c293e4be492dfc54f441e876726f095e0
Mihir Bommisetty <38711080+munnamihir@users.noreply.github.com>
2026-04-01 20:32:58 -0400

    Update devices.js

--- server/routes/devices.js
+++ server/routes/devices.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { pool } from "../db.js";
 import { requireAuth } from "../server.js";
+import crypto from "crypto";
 
 export const deviceRoutes = express.Router();
 
